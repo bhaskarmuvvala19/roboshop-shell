67244b0bf7c4b3e09ca5d7675704f1b47dc57055
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-03 18:09:12 +0530

    added source.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,22 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
+source common.sh
 
-#update file /etc/mongod.conf from 127.0.0.1 to 0.0.0.0
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+rm -rf /app/*
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+cp configs/catalogue.service /etc/systemd/system/catalogue.service
 
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y
+
+mongo --host mongodb.devopsb71.online </app/schema/catalogue.js
