6dea50eaee11870bb39f1626648b24a74db7e739
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-03 18:43:00 +0530

    added sed

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,16 @@
 source common.sh
 
 print_head "Setup MongoDB repository"
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "Install MongoDB"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log_file}
+
+print_head "Update MongoDB Listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 
 print_head "Enable MongoDB"
-systemctl enable mongod
+systemctl enable mongod &>>${log_file}
 
 print_head "Start MongoDB Service"
-systemctl start mongod
-
-# Update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
+systemctl restart mongod &>>${log_file}
