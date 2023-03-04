96491e66c1595c35f5fee1396da794c2119df39a
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-04 15:39:25 +0530

    added

--- catalogue.sh
+++ catalogue.sh
@@ -9,11 +9,16 @@ yum install nodejs -y &>>${log_file}
 status_check $?
 
 print_head "Create Roboshop User"
-useradd roboshop &>>${log_file}
+id roboshop  &>>${log_file}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${log_file}
+fi
 status_check $?
 
 print_head "Create Application Directory"
-mkdir /app &>>${log_file}
+if [ ! -d /app ]; then
+  mkdir /app &>>${log_file}
+fi
 status_check $?
 
 print_head "Delete Old Content"
@@ -59,4 +64,4 @@ status_check $?
 
 print_head "Load Schema"
 mongo --host mongodb.devopsb71.online </app/schema/catalogue.js &>>${log_file}
-status_check $?
+status_check $
